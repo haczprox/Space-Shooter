afda19ba4cf631ac17dc268acb655289b487f13d
Luis Ventura <lfav@student.dei.uc.pt>
2019-10-16 00:57:13 +0100

    Start new Project
    Simple movement only

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    [SerializeField]  
+    private float playerMoveSpeed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the Player starting position to (0, 0, 0)
+        gameObject.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movementDirectionVector = new Vector3(horizontalInput, verticalInput, 0);
+        gameObject.transform.Translate(Time.deltaTime * playerMoveSpeed * movementDirectionVector);
+    }
+}
