a8b2fd4edaae75eb39e5de154c41163a7aa9fa5f
haczprox <haczprox@gmail.com>
2019-10-17 18:05:20 +0100

    Laser shooting

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float laserMoveSpeed = 5f;
+
+    public float GetLaserHeight()
+    {
+        RectTransform rt = (RectTransform) gameObject.transform;
+
+        return rt.rect.height;
+    }
+    
+    void Update() 
+    {
+        MoveOrDestroy();
+    }
+
+    private void MoveOrDestroy()
+    {
+        if (transform.position.y >= 4.9f) Destroy(gameObject);
+        
+        gameObject.transform.Translate(Time.deltaTime * laserMoveSpeed * Vector3.up);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-    [SerializeField]  
-    private float playerMoveSpeed = 5f;
+    [SerializeField] private float playerMoveSpeed = 5f;
+
+    [SerializeField] private GameObject laserPrefab;
     
     // Start is called before the first frame update
     void Start()
@@ -18,10 +19,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserPrefab, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
+
         Vector3 movementDirectionVector = new Vector3(horizontalInput, verticalInput, 0);
         gameObject.transform.Translate(Time.deltaTime * playerMoveSpeed * movementDirectionVector);
+
+        var playerPosition= transform.position;
+        transform.position = new Vector3(Mathf.Clamp(playerPosition.x, -9.2f, 9.2f), Mathf.Clamp(playerPosition.y, -4.9f, 4.9f), 0);
     }
 }
